fca597d36ec5ac3b0043f8510ce86e3f74016086
marccz01 <nelson.marc5678@gmail.com>
2025-10-23 01:27:37 +0700

    Create userModel.js

---
+++ models/userModel.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: [true, "Username wajib di isi"],
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: [true, "Email wajib di isi"],
+            unique: true,
+            trim: true
+        },
+        password: {
+            type: String,
+            required: [true, "Password wajib di isi"]
+        },
+    },
+    {
+        timestamps: true
+    }
+)
+
+
+const User = mongoose.model("User", UserSchema)
+
+export default User
