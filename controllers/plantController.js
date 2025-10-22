91599bf265ea8ef04e0a18ca1334db2142ba5647
marccz01 <nelson.marc5678@gmail.com>
2025-10-23 01:27:28 +0700

    Create plantController.js

---
+++ controllers/plantController.js
@@ -0,0 +1,9 @@
+import plantModel from "../models/plantModel.js"
+
+export const createPlant = async (req, res) => {}
+
+export const updatePlant = async (req, res) => {}
+
+export const listPlant = async (req, res) => {}
+
+export const deletePlant = async (req, res) => {}
