d60cd862522e8a5d6ad41a37fb5c7f1722138e91
marccz01 <nelson.marc5678@gmail.com>
2025-10-23 11:13:27 +0700

    Update plantController.js

--- controllers/plantController.js
+++ controllers/plantController.js
@@ -1,9 +1,109 @@
 import plantModel from "../models/plantModel.js"
 
-export const createPlant = async (req, res) => {}
+export const listPlant = async (req,res) => {
+    try {
+        const data = await plantModel.find({})
 
-export const updatePlant = async (req, res) => {}
+        res.status(200).json({
+            tanaman : "Jadwal Tanaman",
+            data : data
+        })
+    } catch (error) {
+        res.status(500).json({
+            tanaman : error,
+            data : null
+        })
+    }
+}
 
-export const listPlant = async (req, res) => {}
+export const createPlant = async (req,res) => {
+    try {
+        const request = req.body
 
-export const deletePlant = async (req, res) => {}
+        const response = await plantModel.create({
+            plantNameame : request.plantName,
+            species : request.species,
+            lastWatered : request.date
+        })
+
+        res.status(201).json({
+            tanaman : "Data pelacakan tanaman berhasil dibuat",
+            data : response
+        })
+        
+    } catch (error) {
+        res.status(500).json({
+            tanaman : error,
+            data : null
+        })
+    }
+}
+
+export const updatePlant = async (req,res) => {
+    try {
+        const id = req.params?.id
+        const request = req.body
+
+        if(!id){
+            return res.status(500).json({
+                tanaman : "Id wajib di isi",
+                data : null
+            })
+        }
+
+         const response = await plantModel.findByIdAndUpdate(id, {
+            plantName : request.plantName,
+            lastWatered : request.lastWatered
+         })
+
+        if(!response){
+            return res.status(500).json({
+                tanaman : "Data pelacakan tanaman gagal diupdate",
+                data : null
+            })
+        }
+
+        return res.status(200).json({
+            tanaman : "Data pelacakan tanaman berhasil diupdate"
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            tanaman : error,
+            data : null
+        })
+    }
+}
+
+export const deletePlant = async (req,res) => {
+    try {
+        const id = req.params.id
+
+        if(!id){
+            return res.status(500).json({
+                tanaman : "Id wajib diisi",
+                data : null
+            })
+        }
+
+        const response = await plantModel.findByIdAndDelete(id)
+
+        if(response){
+            return res.status(200).json({
+                tanaman : "Data pelacakan tanaman berhasil dihapus",
+                data : null
+            })
+        }
+
+        return res.status(404).json({
+                tanaman : "Data pelacakan tanaman tidak ditemukan",
+                data : null
+            })
+
+    } catch (error) {
+        res.status(500).json({
+            tanaman : error,
+            data : null
+        })
+    }
+}
