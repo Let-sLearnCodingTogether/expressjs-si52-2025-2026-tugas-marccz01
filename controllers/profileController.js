d36251d4bfc25fb77db05af1ac058b44eb7aa724
marccz01 <nelson.marc5678@gmail.com>
2025-10-23 11:13:41 +0700

    Create profileController.js

---
+++ controllers/profileController.js
@@ -0,0 +1,13 @@
+export const privateProfile = async (req, res) => {
+    try {
+        res.status(200).json({
+            message: "Private profile berhasil di akses",
+            data: null,
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+            data: null,
+        });
+    }
+};
