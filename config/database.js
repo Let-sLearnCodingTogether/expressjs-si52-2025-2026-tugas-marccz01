076b6f38e41a4faf58cf2f1c847623f1c610f7c6
marccz01 <nelson.marc5678@gmail.com>
2025-10-23 11:12:36 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,16 +1,15 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose"
 
-const database = async () => {
+export const database = async () => {
     try {
-        console.log("Melakukan koneksi ke mongodb");
+        console.log("Starting connection to mongodb...");
 
         const response = await mongoose.connect("mongodb://127.0.0.1:27017/Nelson?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.5.8")
 
-        console.log(`Koneksi ke mongodb berhasil host : ${response.connection.host}`);
-    } catch (error){
-        console.error("Gagal terkoneksi dengan mongodb");
+        console.log(`Connected succesfully to mongodb at : ${response.connection.host}`);
+    } catch (error) {
+        console.log("Mongodb connection failed");
+
         process.exit(1)
     }
 }
-
-export default database
