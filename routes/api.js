55497a5975c8dd9a9a1e0e19685044a83a7a0cca
marccz01 <nelson.marc5678@gmail.com>
2025-10-26 15:44:58 +0700

    update

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 import express from "express";
 import * as authController from "../controllers/authController.js";
+import * as plantController from "../controllers/plantController.js";
 import * as profileController from "../controllers/profileController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
@@ -8,6 +9,11 @@ const api = express.Router();
 api.post('/register', authController.register);
 api.post('/login', authController.login);
 
-api.get('/me', protect, profileController.privateProfile);
+api.get('/profile', protect, profileController.privateProfile);
+
+api.post("/plant", plantController.createPlant);
+api.get("/plant", plantController.listPlant);
+api.put("/plant/:id", plantController.updatePlant);
+api.delete("/plant/:id", plantController.deletePlant);
 
 export default api;
