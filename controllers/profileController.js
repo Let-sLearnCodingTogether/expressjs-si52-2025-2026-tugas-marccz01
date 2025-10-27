b25f455b9e834c3ea1094142d15d1110179048f8
lollyy28 <nelson.marc5678@gmail.com>
2025-10-27 23:02:14 +0700

    Update profileController.js

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,13 +1,25 @@
+export const publicProfile = (req, res) => {
+const username = req.params.username
+    res.render('public-profile', {
+        title : username,
+        username : username,
+        bio: "test 1234"
+    })
+}
+
 export const privateProfile = async (req, res) => {
     try {
+        const user = req.user;
+
         res.status(200).json({
             message: "Private profile berhasil di akses",
-            data: null,
+            data: user,
         });
+        
     } catch (error) {
         res.status(500).json({
             message: error.message,
             data: null,
         });
     }
-}
+};
